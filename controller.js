c595996da546333a44671fad35e5ec8361fd7570
danikusuma10 <kusumadanikusuma@outlook.com>
2020-05-08 21:47:38 +0700

    Akses Public

--- controller.js
+++ controller.js
@@ -31,28 +31,7 @@ exports.tampilsemuadatamontir = function (req, res) {
     });
 };
 
-//GET data Tabel t_user
-exports.tampilsemuadatauser = function (req, res) {
-    connection.query('SELECT * FROM t_user', function (error, rows, fields) {
-        if (error) {
-            console.log(error);
-        }
-        else {
-            response.ok(rows, res)
-        }
-    });
-};
-//GET data Tabel t_service
-exports.tampilsemuadataservice = function (req, res) {
-    connection.query('SELECT * FROM t_service', function (error, rows, fields) {
-        if (error) {
-            console.log(error);
-        }
-        else {
-            response.ok(rows, res)
-        }
-    });
-};
+
 
 //-----------------------------------------------------------------------------------------------
 
@@ -93,92 +72,4 @@ exports.tampildtmontirdasarid = function (req, res) {
             }
 
         });
-};
-
-//GET data Tabel t_user berdasar id
-exports.tampildtuserdasarid = function (req, res) {
-
-    let id = req.params.id;
-    connection.query('SELECT * FROM t_user WHERE id=?', [id],
-        function (error, rows, fields) {
-            if (error) {
-                console.log(error);
-            }
-            else {
-                response.ok(rows, res)
-            }
-
-        });
-};
-
-//GET data Tabel t_service berdasar id
-exports.tampildtservicedasarid = function (req, res) {
-
-    let id = req.params.id;
-    connection.query('SELECT * FROM t_service WHERE id=?', [id],
-        function (error, rows, fields) {
-            if (error) {
-                console.log(error);
-            }
-            else {
-                response.ok(rows, res)
-            }
-
-        });
-};
-
-//----------------------------------------------------------------------------------------------
-
-//------------------------------------"TAMBAH DATA"---------------------------------------
-
-
-//tambahkan data tbl_user
-exports.tambahuser = function (req, res) {
-
-    var iduser = req.body.iduser;
-    var nama_user = req.body.nama_user;
-    var email = req.body.email;
-    var password = req.body.password;
-    var level = req.body.level;
-    
-    connection.query('INSERT INTO t_user (iduser,nama_user,email,password,level) VALUES(?,?,?,?,?)',
-    [iduser,nama_user,email,password,level],
-        function (error, rows, fields) {
-            if (error) {
-                console.log(error);
-            } else {
-                response.ok("Berhasil menambah Data", res)
-            }
-        });
-};
-
-
-
-exports.tambahSparepart = function (req, res) {
-    var id_sparepart= req.body.id_sparepart;
-    var nama_sparepart = req.body.nama_sparepart;
-    var harga_sparepart = req.body.harga_sparepart;
-    var satuan = req.body.satuan;
-
-    connection.query('INSERT INTO t_sparepart (id_sparepart,nama_sparepart,harga_sparepart,satuan) VALUES(?,?,?)',
-
-        [id_sparepart,nama_sparepart,harga_sparepart,satuan],
-        function (error, rows, fields) {
-
-            if (error) {
-
-                console.log(error);
-
-            } else {
-
-                response.ok("Berhasil menambahkan data!", res);
-
-            }
-
-        });
-
-
-
-
-        
 };
--- routes.js
+++ routes.js
@@ -9,25 +9,17 @@ module.exports = function(app){
 //tampil semua data
     app.route('/tampilsemuasparepart')
         .get(jsonku.tampilsemuadatasparepart);
-    app.route('/tampilsemuauser')
-        .get(jsonku.tampilsemuadatauser);
+    
     app.route('/tampilsemuamontir')
         .get(jsonku.tampilsemuadatamontir);
-    app.route('/tampilsemuaservice')
-        .get(jsonku.tampilsemuadataservice);
+   
 
 //tampil berdasar id
     app.route('/tampilsparepart/:id')
         .get(jsonku.tampildtsparepartdasarid)
-    app.route('/tampiluser/:id')
-        .get(jsonku.tampildtuserdasarid)
+    
     app.route('/tampilmontir/:id')
         .get(jsonku.tampildtmontirdasarid)
-    app.route('/tampilservice/:id')
-        .get(jsonku.tampildtservicedasarid)
 
-//tambah data
-app.route('/tambah')
-    .post(jsonku.tambahuser)
    
 }
