b3f23396a9b348b9e5307ccaec28eee48086110b
danikusuma10 <kusumadanikusuma@outlook.com>
2020-05-12 16:10:51 +0700

    update db

--- DB_BENGKEL.sql
+++ DB_BENGKEL.sql
@@ -16,6 +16,21 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`uts_bengkel` /*!40100 DEFAULT CHARACTER
 
 USE `uts_bengkel`;
 
+/*Table structure for table `akses_token` */
+
+DROP TABLE IF EXISTS `akses_token`;
+
+CREATE TABLE `akses_token` (
+  `id_user` varchar(200) NOT NULL,
+  `access_token` varbinary(200) NOT NULL,
+  `ip_address` int(200) NOT NULL,
+  KEY `id_user` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `akses_token` */
+
+insert  into `akses_token`(`id_user`,`access_token`,`ip_address`) values ('A-2','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb3dzIjpbeyJpZF91c2VyIjoiQS0yIiwibmFtYV91c2VyIjoiZGFuIiwiZW1haWwiOiJkYW5AbWFpbC5jb20iLCJwYXNzd29yZCI6IjkxODBiNGRhM2YwYzdlODA5NzVmYWQ2ODVmN2YxMzRlIiwiaWRfbGV2ZWw',192168),('A-3','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb3dzIjpbeyJpZF91c2VyIjoiQS0zIiwibmFtYV91c2VyIjoiYmJiIiwiZW1haWwiOiJiYmJAbWFpbC5jb20iLCJwYXNzd29yZCI6IjA4ZjhlMDI2MGM2NDQxODUxMGNlZmIyYjA2ZWVlNWNkIiwiaWRfbGV2ZWw',192168),('A-3','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb3dzIjpbeyJpZF91c2VyIjoiQS0zIiwibmFtYV91c2VyIjoiYmJiIiwiZW1haWwiOiJiYmJAbWFpbC5jb20iLCJwYXNzd29yZCI6IjA4ZjhlMDI2MGM2NDQxODUxMGNlZmIyYjA2ZWVlNWNkIiwiaWRfbGV2ZWw',192168),('A-4','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb3dzIjpbeyJpZF91c2VyIjoiQS00IiwibmFtYV91c2VyIjoieHh4IiwiZW1haWwiOiJ4eHhAbWFpbC5jb20iLCJwYXNzd29yZCI6IjA4ZjhlMDI2MGM2NDQxODUxMGNlZmIyYjA2ZWVlNWNkIiwiaWRfbGV2ZWw',192168),('A-2','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb3dzIjpbeyJpZF91c2VyIjoiQS0yIiwibmFtYV91c2VyIjoiZGFuIiwiZW1haWwiOiJkYW5AbWFpbC5jb20iLCJwYXNzd29yZCI6IjkxODBiNGRhM2YwYzdlODA5NzVmYWQ2ODVmN2YxMzRlIiwiaWRfbGV2ZWw',192168);
+
 /*Table structure for table `t_level` */
 
 DROP TABLE IF EXISTS `t_level`;
@@ -43,6 +58,8 @@ CREATE TABLE `t_montir` (
 
 /*Data for the table `t_montir` */
 
+insert  into `t_montir`(`id_montir`,`nama_montir`,`hargaperjam`) values ('M-01','jono',30000);
+
 /*Table structure for table `t_service` */
 
 DROP TABLE IF EXISTS `t_service`;
@@ -56,15 +73,17 @@ CREATE TABLE `t_service` (
   `jumlah_sparepart` int(200) NOT NULL,
   `total_service` int(200) NOT NULL,
   PRIMARY KEY (`id_service`),
-  KEY `id_user` (`id_user`),
   KEY `id_montir` (`id_montir`),
+  KEY `id_user` (`id_user`),
   KEY `id_sparepart` (`id_sparepart`),
-  CONSTRAINT `t_service_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `t_user` (`id_user`),
-  CONSTRAINT `t_service_ibfk_2` FOREIGN KEY (`id_montir`) REFERENCES `t_montir` (`id_montir`),
+  CONSTRAINT `t_service_ibfk_1` FOREIGN KEY (`id_montir`) REFERENCES `t_montir` (`id_montir`),
+  CONSTRAINT `t_service_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `t_user` (`id_user`),
   CONSTRAINT `t_service_ibfk_3` FOREIGN KEY (`id_sparepart`) REFERENCES `t_sparepart` (`id_sparepart`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `t_service` */
+
+insert  into `t_service`(`id_service`,`tgl_service`,`id_user`,`id_montir`,`id_sparepart`,`jumlah_sparepart`,`total_service`) values ('S-01','2020-05-12','A-2','M-01','O-01',5,0);
 
 /*Table structure for table `t_sparepart` */
 
@@ -79,6 +98,8 @@ CREATE TABLE `t_sparepart` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `t_sparepart` */
+
+insert  into `t_sparepart`(`id_sparepart`,`nama_sparepart`,`harga_sparepart`,`satuan`) values ('O-01','OLI',2000,20);
 
 /*Table structure for table `t_user` */
 
@@ -97,7 +118,7 @@ CREATE TABLE `t_user` (
 
 /*Data for the table `t_user` */
 
-insert  into `t_user`(`id_user`,`nama_user`,`email`,`password`,`id_level`) values ('U-01','Kusuma','mail@mail.com','ef86916bc6f9f2f6866df100a192161f',1),('U-02','Dani','kusuma@mail.com','ef86916bc6f9f2f6866df100a192161f',2),('U-06','KAKA','kaka@mail.com','5541c7b5a06c39b267a5efae6628e003',1);
+insert  into `t_user`(`id_user`,`nama_user`,`email`,`password`,`id_level`) values ('A-1','alfida','alfida@mail.com','d32b7e3369d2053cf393693e800f879a',2),('A-2','dan','dan@mail.com','9180b4da3f0c7e80975fad685f7f134e',2),('A-3','bbb','bbb@mail.com','08f8e0260c64418510cefb2b06eee5cd',2),('A-4','xxx','xxx@mail.com','08f8e0260c64418510cefb2b06eee5cd',2),('U-01','Kusuma','mail@mail.com','ef86916bc6f9f2f6866df100a192161f',1),('U-02','Dani','kusuma@mail.com','ef86916bc6f9f2f6866df100a192161f',2),('U-06','KAKA','kaka@mail.com','5541c7b5a06c39b267a5efae6628e003',1),('U-1000','Dani','danikusuma@mail.com','ab5e831d8600f15167307bd73177b525',1),('U-111','kakao','kakao@mail.com','d42de1f57e8dd01202d9c36d807fb157',2),('U-18','aaa','aaa@mail.com','47bce5c74f589f4867dbd57e9ca9f808',2),('U-90','sss','sss@mail.com','9f6e6800cfae7749eb6c486619254b9c',2);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
