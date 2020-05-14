8e21fb0d9fee82160ea3d434976a4de291a77f30
danikusuma10 <kusumadanikusuma@outlook.com>
2020-05-14 12:43:29 +0700

    success user t_service

--- middleware/index.js
+++ middleware/index.js
@@ -2,14 +2,13 @@ var express = require('express');
 var auth = require('./auth');
 var router = express.Router();
 
-//defined
 var verifadmin = require('./verifikasi');
 var verifuser = require('./verifikasi');
 
-var verifikasi.verifadmin()
+
 //==============================================================MENU REGISTRASI USER===============================
 router.post('/api/v1/register', auth.register);
-router.post('/api/v1/login', auth.login);
+router.post('/api/v1/login', auth .login);
 
 //alamat yang perlu otorisasi
 router.get('/api/v1/rahasia',verifuser(), auth.loginuser);
--- middleware/verifikasi.js
+++ middleware/verifikasi.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/secret');
 
 
+
 function verifadmin(){
     return function(req, rest, next){
         var lvl = req.body.lvl;
@@ -13,18 +14,18 @@ function verifadmin(){
             //verifikasi
             jwt.verify(token, config.secret, function(err, decoded){
                 if(err){
-                    return rest.status(401).send({auth:false, message:'Token tidak terdaftar!'});
+                    return rest.status(401).send({auth:false, message:'No Detected Token!'});
                 }else {
                     if(lvl == 1 ){
                         req.auth = decoded;
                         next();
                     }else {
-                        return rest.status(401).send({auth:false, message:'FAILED authorize admin !'});
+                        return rest.status(401).send({auth:false, message:'FAILED authorize Youre User !'});
                     }
                 }
             });
         }else {
-            return rest.status(401).send({auth:false, message:'Token tidak tersedia!'});
+            return rest.status(401).send({auth:false, message:'Token Not Available'});
         }
     }
 }
@@ -43,18 +44,18 @@ function verifuser(){
             //verifikasi
             jwt.verify(token, config.secret, function(err, decoded){
                 if(err){
-                    return rest.status(401).send({auth:false, message:'Token tidak terdaftar!'});
+                    return rest.status(401).send({auth:false, message:'No Detected Token'});
                 }else {
                     if(lvl == 2 ){
                         req.auth = decoded;
                         next();
                     }else {
-                        return rest.status(401).send({auth:false, message:'FAILED AUTHORIZATION'});
+                        return rest.status(401).send({auth:false, message:'FAILED AUTHORIZATION Youre Admin !'});
                     }
                 }
             });
         }else {
-            return rest.status(401).send({auth:false, message:'Token tidak tersedia!'});
+            return rest.status(401).send({auth:false, message:'Token Not available'});
         }
     }
 }
